6bf22965b49daf9388b79a8784bb66b7f975dd12
elodieT <elodie.thieblin@laposte.net>
2015-03-31 17:12:14 +0200

    helloack et info

--- app/src/main/java/com/message/Informations.java
+++ app/src/main/java/com/message/Informations.java
@@ -4,6 +4,7 @@ package com.message;
  * Created by lucille on 24/03/15.
  */
 public class Informations extends AbstractMessage{
+    //Mettre les objets de position,
 
     public Informations(){
         this.type=typeContenu.INFORMATIONS;
