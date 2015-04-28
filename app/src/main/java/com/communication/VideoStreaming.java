daaf8ca556b93fad02b8a37b8fe82d60a70d1988
elodieT <elodie.thieblin@laposte.net>
2015-04-28 15:20:30 +0200

    rtps

---
+++ app/src/main/java/com/communication/VideoStreaming.java
@@ -0,0 +1,21 @@
+package com.communication;
+import android.net.rtp.RtpStream;
+/**
+ * Created by sony on 28/04/2015.
+ * TOUJOURS EN COURS, JUSTE UNE EBAUCHE
+ */
+public class VideoStreaming {
+    RtpStream rtps ;
+
+    VideoStreaming(boolean drone){
+
+        if(drone){
+            rtps.setMode(RtpStream.MODE_SEND_ONLY);
+        }
+        else{
+            rtps.setMode(RtpStream.MODE_RECEIVE_ONLY);
+        }
+    }
+
+}
+
